8ffbe3bc3a8f3805105b1af5907333853ad3765f
Michael Schwartz <mykesx@gmail.com>
2015-02-15 08:29:55 -0800

    SockJS in progress

---
+++ SockJS/app.js
@@ -0,0 +1,43 @@
+/**
+ * Created by mschwartz on 12/15/14.
+ */
+
+/*global require, global */
+
+var SockJS       = require('decaf-jolt-sockjs').SockJS,
+    Application  = require('decaf-jolt').Application,
+    StaticFile   = require('decaf-jolt-static').StaticFile,
+    StaticServer = require('decaf-jolt-static').StaticServer,
+    //SjsFile      = require('decaf-jolt-sjs').SjsFile,
+    //RestServer   = require('decaf-jolt-rest').RestServer,
+    app          = new Application(),
+    sockjs       = new SockJS(app, 'echo'),
+    {setInterval, clearInterval}  = require('Timers');
+
+app.verb('/', new StaticFile('index.html'));
+app.verb('bower_components', new StaticServer('bower_components'));
+
+sockjs.on('open', function (sock) {
+    console.log('==== open ====');
+    var i = 1;
+    var id = setInterval(function () {
+        var s = 'ping ' + i++;
+        console.log(s);
+        sock.write(s);
+    }, 1000);
+    sock.on('close', function () {
+        console.log('==== close ====');
+        clearInterval(id);
+    });
+    sock.on('message', function (message) {
+        console.log('==== message ====');
+        console.dir({
+            message : message
+        });
+        sock.write(message);
+    });
+    debugger;
+});
+
+app.listen(8081, '0.0.0.0');
+console.log('app running on port 8081');
